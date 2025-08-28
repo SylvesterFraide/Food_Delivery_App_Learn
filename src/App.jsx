a8e129c648b9e66c7f34c55c33a25c73591a1b5c
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-28 20:59:20 +0300

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route  } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Cart from './Pages/Cart/Cart';
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
@@ -12,6 +16,11 @@ function App() {
         contactUs="Contact Us"
         signIn="Sign In"
       />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,16 +4,35 @@ import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 import ShoppingBasketTwoToneIcon from "@mui/icons-material/ShoppingBasketTwoTone";
 
 const Navbar = ({ mylogo, home, menu, mobileApp, contactUs, signIn }) => {
-
-    const [activeMenu, setActiveMenu] = useState('home');
+  const [activeMenu, setActiveMenu] = useState("home");
   return (
     <div className="navbar">
       <h3 className="logo">{mylogo}</h3>
       <ul className="navbar-menu">
-        <li className={activeMenu === 'home' ? 'active' : ''} onClick={() => setActiveMenu('home')}>{home}</li>
-        <li className={activeMenu === 'menu' ? 'active' : ''} onClick={() => setActiveMenu('menu')}>{menu}</li>
-        <li className={activeMenu === 'mobileApp' ? 'active' : ''} onClick={() => setActiveMenu('mobileApp')}>{mobileApp}</li>
-        <li className={activeMenu === 'contactUs' ? 'active' : ''} onClick={() => setActiveMenu('contactUs')}>{contactUs}</li>
+        <li
+          className={activeMenu === "home" ? "active" : ""}
+          onClick={() => setActiveMenu("home")}
+        >
+          {home}
+        </li>
+        <li
+          className={activeMenu === "menu" ? "active" : ""}
+          onClick={() => setActiveMenu("menu")}
+        >
+          {menu}
+        </li>
+        <li
+          className={activeMenu === "mobileApp" ? "active" : ""}
+          onClick={() => setActiveMenu("mobileApp")}
+        >
+          {mobileApp}
+        </li>
+        <li
+          className={activeMenu === "contactUs" ? "active" : ""}
+          onClick={() => setActiveMenu("contactUs")}
+        >
+          {contactUs}
+        </li>
       </ul>
       <div className="navbar-right">
         <SearchTwoToneIcon />
