a038196f6e706ca55b4440230e8f051a422bc1a0
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-28 22:23:02 +0300

    styled header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-content">
+      <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>
           Choose from a diverse menu featuring a delectable array off dishes
