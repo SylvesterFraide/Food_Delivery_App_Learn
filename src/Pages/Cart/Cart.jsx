75437bd25ce2c1612b59cae3d95499368b6daad6
SylvesterFraide <fridaysylvester2@gmail.com>
2025-09-09 21:12:05 +0300

    18th commit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from 'react'
-import './Cart.css';
-import { StoreContext } from '../../Context/StoreContext';
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../Context/StoreContext";
 import CloseIcon from "@mui/icons-material/Close";
 
 const Cart = () => {
-
-   const {cartItems, removeFromCart, Food_List} = useContext(StoreContext);
+  const { cartItems, removeFromCart, Food_List } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -20,27 +19,27 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {
-          Food_List.map((item, index) => {
-            if(cartItems[item.id] > 0) {
-              return (
-                <div className="cart-items-title">
-                  <img src={item.image} alt='' />
+        {Food_List.map((item, index) => {
+          if (cartItems[item.id] > 0) {
+            return (
+              <>
+                <div className="cart-items-title cart-items-items">
+                  <img src={item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>${item.price}</p>
                   <p>{cartItems[item.id]}</p>
-                  <p>{item.price * cartItems[item.id]}</p>
-                  <CloseIcon />
+                  <p>${item.price * cartItems[item.id]}</p>
+                  <CloseIcon className="close" onClick={() => removeFromCart(item.id)} />
                   {/* <button onClick={() => removeFromCart(item.id)}>Remove</button> */}
                 </div>
-              )
-            }
-          })
-        }
-
+                <hr />
+              </>
+            );
+          }
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
